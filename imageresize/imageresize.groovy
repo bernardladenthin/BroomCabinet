c7e566f44a76cc99cc97b9411e55486436a7b554
Bernard Ladenthin <bernard.ladenthin@gmail.com>
2017-12-19 19:40:49 +0100

    allow to set compressionQuality

--- imageresize/imageresize.groovy
+++ imageresize/imageresize.groovy
@@ -1,13 +1,17 @@
-import java.awt.image.BufferedImage;
-import java.awt.Image;
-import java.awt.Dimension;
-import java.awt.Graphics;
-import java.io.File;
-import javax.imageio.ImageIO;
-import javax.swing.ImageIcon;
+import javax.imageio.IIOImage
+import javax.imageio.ImageIO
+import javax.imageio.ImageWriteParam
+import javax.imageio.ImageWriter
+import javax.imageio.stream.FileImageOutputStream
+import javax.imageio.stream.ImageOutputStream
+import javax.swing.*
+import java.awt.*
+import java.awt.image.BufferedImage
+import java.io.File
 
-def maxWidth = 360;
-def maxHeight = 360;
+def maxWidth = 360
+def maxHeight = 360
+def compressionQuality = 0.95f
 
 // https://stackoverflow.com/questions/10245220/java-image-resize-maintain-aspect-ratio
 // SNIP
@@ -41,24 +45,36 @@ def Dimension getScaledDimension(Dimension imgSize, Dimension boundary) {
 // SNAP
 
 def resize(file, maxDimension) {
-    Image img = new ImageIcon(ImageIO.read(file)).getImage();
-    Dimension imgDimension = new Dimension(img.getWidth(null), img.getHeight(null));
-    
-    Dimension newDimension = getScaledDimension(imgDimension, maxDimension);
-    
-    Image scaledImage = img.getScaledInstance(newDimension.@width, newDimension.@height, Image.SCALE_SMOOTH);
-    BufferedImage outImg = new BufferedImage(newDimension.@width, newDimension.@height, BufferedImage.TYPE_INT_RGB);
-    Graphics g = outImg.getGraphics();
-    g.drawImage(scaledImage, 0, 0, null);
-    g.dispose();
-    ImageIO.write(outImg, "jpeg", file);
+    Image img = new ImageIcon(ImageIO.read(file)).getImage()
+    Dimension imgDimension = new Dimension(img.getWidth(null), img.getHeight(null))
+
+    Dimension newDimension = getScaledDimension(imgDimension, maxDimension)
+
+    Image scaledImage = img.getScaledInstance(newDimension.@width, newDimension.@height, Image.SCALE_SMOOTH)
+    BufferedImage outImg = new BufferedImage(newDimension.@width, newDimension.@height, BufferedImage.TYPE_INT_RGB)
+    Graphics g = outImg.getGraphics()
+    g.drawImage(scaledImage, 0, 0, null)
+    g.dispose()
+
+    // https://stackoverflow.com/questions/17108234/setting-jpg-compression-level-with-imageio-in-java
+    ImageWriter jpgWriter = ImageIO.getImageWritersByFormatName("jpg").next()
+    ImageWriteParam jpgWriteParam = jpgWriter.getDefaultWriteParam()
+    jpgWriteParam.setCompressionMode(ImageWriteParam.MODE_EXPLICIT)
+    jpgWriteParam.setCompressionQuality(compressionQuality)
+
+    ImageOutputStream outputStream = new FileImageOutputStream(file)
+    jpgWriter.setOutput(outputStream)
+
+    jpgWriter.write(null, new IIOImage(outImg, null, null), jpgWriteParam)
+    jpgWriter.dispose()
 }
 
 // main loop
+Dimension dimension = new Dimension(maxWidth, maxHeight)
 new File('.').eachFileRecurse {
-    def isJpeg = it.name.toLowerCase().endsWith(".jpg")
+    def isJpeg = it.name.toLowerCase().endsWith(".jpg") || it.name.toLowerCase().endsWith(".jpeg")
     if (isJpeg) {
-        println "ok, resize: " + it.name;
-        resize(it, new Dimension(maxWidth, maxHeight));
+        println it.name
+        resize(it, dimension)
     }
 }
