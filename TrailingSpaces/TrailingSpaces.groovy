c88ac91e73d4bd68d4569972eafad6fb45c12cf7
Bernard Ladenthin <bernard.ladenthin@gmail.com>
2018-01-22 12:22:40 +0100

    improve TrailingSpaces.groovy

--- TrailingSpaces/TrailingSpaces.groovy
+++ TrailingSpaces/TrailingSpaces.groovy
@@ -2,15 +2,15 @@ new File('.').eachFileRecurse {
     boolean ok = true;
     if (it.name.startsWith (" ") ) {
         ok = false;
-        println "startsWith space: " + it.name
+        println "nok: startsWith space: " + it.name
     }
     if (it.name.endsWith (" ") ) {
         ok = false;
-        println "endsWith space: " + it.name
+        println "nok: endsWith space: " + it.name
     }
     if (it.name.contains ("  ") ) {
         ok = false;
-        println "contains double spaces: " + it.name
+        println "nok: contains double spaces: " + it.name
     }
     
     if (ok) {
